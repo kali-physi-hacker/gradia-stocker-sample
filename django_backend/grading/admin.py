667d18d37827dceb77f4196d36b3747edd5643b2
conrad <conrad@pythonanywhere.com>
2020-06-01 16:16:05 +0800

    add more stuff to the purchases admin

--- django_backend/grading/admin.py
+++ django_backend/grading/admin.py
@@ -5,6 +5,20 @@ from django.contrib import admin
 from .models import Parcel, Receipt
 
 
+@admin.register(Parcel)
+class ParcelAdmin(admin.ModelAdmin):
+    model = Parcel
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 class CreateReceipt(Receipt):
     class Meta:
         proxy = True
@@ -36,31 +50,6 @@ class CreateReceiptAdmin(admin.ModelAdmin):
         obj.intake_by = request.user
         obj.save()
 
-    """
-    def save_formset(self, request, form, formset, change):
-        instances = formset.save(commit=False)
-
-        for instance in instances:
-            if isinstance(instance, Parcel):
-                instance.intake_by = request.user
-                instance.save()
-            print(instance)
-    """
-
-
-@admin.register(Parcel)
-class ParcelAdmin(admin.ModelAdmin):
-    model = Parcel
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
 
 class CloseOutReceipt(Receipt):
     class Meta:
@@ -68,25 +57,19 @@ class CloseOutReceipt(Receipt):
         verbose_name = "2. close out receipt for stone release"
 
 
-class ParcelOutInline(admin.StackedInline):
-    model = Parcel
-    fields = ["name", "code", "total_carats", "total_pieces"]
-    readonly_fields = ["name", "code", "total_carats", "total_pieces"]
-    extra = 0
+def close_out(modeladmin, request, queryset):
+    queryset.update(release_by=request.user, release_date=datetime.now())
 
-    def has_add_permission(self, request, obj=None):
-        return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+close_out.short_description = "Close out selected recipt"
 
 
 @admin.register(CloseOutReceipt)
 class CloseOutReceiptAdmin(admin.ModelAdmin):
     model = CloseOutReceipt
-    readonly_fields = ["entity", "code", "intake_by", "intake_date", "release_by", "release_date"]
+    actions = [close_out]
 
-    inlines = [ParcelOutInline]
+    list_filter = ["release_date"]
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -94,7 +77,5 @@ class CloseOutReceiptAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
-    def save_model(self, request, obj, form, change):
-        obj.release_by = request.user
-        obj.release_date = datetime.now()
-        obj.save()
+    def has_change_permission(self, request, obj=None):
+        return False
--- django_backend/purchases/admin.py
+++ django_backend/purchases/admin.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
-from .models import AuthorizedPersonnel, Seller
+from .models import AuthorizedPersonnel, Parcel, Receipt, Seller
 
 
 class AuthorizedPersonnelInline(admin.StackedInline):
@@ -15,3 +18,107 @@ class SellerAdmin(admin.ModelAdmin):
     model = Seller
 
     inlines = [AuthorizedPersonnelInline]
+
+
+class CreateReceipt(Receipt):
+    class Meta:
+        proxy = True
+        verbose_name = "1. Create receipt for stone intake"
+
+
+class ParcelInline(admin.TabularInline):
+    model = Parcel
+    fields = ["name", "code", "total_carats", "total_pieces", "reference_price_per_carat"]
+
+    extra = 1
+
+
+@admin.register(CreateReceipt)
+class CreateReceiptAdmin(admin.ModelAdmin):
+    model = CreateReceipt
+
+    readonly_fields = ["intake_by", "intake_date", "release_by", "release_date"]
+
+    inlines = [ParcelInline]
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        obj.intake_by = request.user
+        obj.save()
+
+
+class ParcelRejection(Parcel):
+    class Meta:
+        proxy = True
+        verbose_name = "2. Parcel rejection"
+
+
+class ParcelRejectionForm(forms.ModelForm):
+    class Meta:
+        model = ParcelRejection
+        fields = ["rejected_carats", "rejected_pieces", "total_price_paid"]
+
+    def clean(self):
+        if self.cleaned_data.get("rejected_carats") is None:
+            raise forms.ValidationError("Can't leave rejected carats field blank")
+        if self.cleaned_data.get("rejected_pieces") is None:
+            raise forms.ValidationError("Can't leave rejected pieces field blank")
+        if self.cleaned_data.get("total_price_paid") is None:
+            raise forms.ValidationError("Can't leave total prices paid field blank")
+        return self.cleaned_data
+
+
+@admin.register(ParcelRejection)
+class ParcelRejectionAdmin(admin.ModelAdmin):
+    model = ParcelRejection
+    form = ParcelRejectionForm
+
+    readonly_fields = ["name", "code", "total_carats", "total_pieces", "reference_price_per_carat"]
+    fields = readonly_fields + ["rejected_carats", "rejected_pieces", "total_price_paid"]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            if obj.rejected_carats is None or obj.total_price_paid is None or obj.rejected_pieces is None:
+                return True
+        return False
+
+
+class CloseOutReceipt(Receipt):
+    class Meta:
+        proxy = True
+        verbose_name = "3. Close out receipt for stone release"
+
+
+def close_out(modeladmin, request, queryset):
+    queryset.update(release_by=request.user, release_date=datetime.now())
+
+
+close_out.short_description = "Close out selected recipt"
+
+
+@admin.register(CloseOutReceipt)
+class CloseOutReceiptAdmin(admin.ModelAdmin):
+    model = CloseOutReceipt
+    actions = [close_out]
+
+    list_filter = ["release_date"]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
