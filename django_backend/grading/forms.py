8638708a381086121c8a005f2887f44e2dc1d9b3
conrad <conrad@pythonanywhere.com>
2020-06-18 14:35:07 +0800

    tweak stone entry form

--- django_backend/grading/forms.py
+++ django_backend/grading/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 number_input = forms.NumberInput(attrs={"style": "width: 50px"})
 text_input = forms.TextInput(attrs={"size": 1})
+longer_text_input = forms.TextInput(attrs={"size": 2})
 text_area = forms.Textarea(attrs={"style": "width: 100px; height: 50px"})
 
 
@@ -12,13 +13,13 @@ class StoneForm(forms.ModelForm):
             "grader_1_color": text_input,
             "grader_2_color": text_input,
             "grader_3_color": text_input,
-            "clarity": text_input,
-            "grader_1_clarity": text_input,
-            "grader_2_clarity": text_input,
-            "grader_3_clarity": text_input,
+            "clarity": longer_text_input,
+            "grader_1_clarity": longer_text_input,
+            "grader_2_clarity": longer_text_input,
+            "grader_3_clarity": longer_text_input,
             "fluo": text_input,
             "culet": text_input,
-            "stone_id": text_input,
+            "stone_id": forms.TextInput(attrs={"size": 8}),
             "sequence_number": number_input,
             "carats": number_input,
             "inclusion_remarks": text_area,
