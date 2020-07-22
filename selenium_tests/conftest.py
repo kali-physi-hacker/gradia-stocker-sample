5c0a1304c4d89ca4ff64de67380c6003961f5d3b
conrad <conrad@pythonanywhere.com>
2020-07-22 10:09:38 +0800

    template example for selenium tests

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -1,10 +1,20 @@
+import time
 from collections import namedtuple
+from functools import partial
+
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 import pytest
+from customers.models import Entity
+from grading.models import Parcel, Receipt
+from ownerships.models import ParcelTransfer
 
 
 def assert_body_contains_text(browser, search_string):
@@ -18,8 +28,42 @@ def assert_body_contains_text(browser, search_string):
 def login(browser, username, password):
     browser.goto("/admin/login/")
     browser.find_element_by_name("username").send_keys(username)
-    browser.find_element_by_name("password").send_keys(password)
-    browser.find_element_by_name("password").send_keys(Keys.RETURN)
+    password_field = browser.find_element_by_name("password")
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    browser.wait_till_gone(password_field)
+
+
+def wait_till_gone(browser, elem):
+    try:
+        WebDriverWait(browser, 2).until(EC.staleness_of(elem))
+    except TimeoutException:
+        raise Exception(f"element did not go away: {elem}")
+
+
+def slowly_click(browser, elem, elem_should_disappear=True):
+    time.sleep(0.5)
+    elem.click()
+    if elem_should_disappear:
+        browser.wait_till_gone(elem)
+    time.sleep(0.3)
+
+
+# TODO: make this use some extended browser class instead
+def setup_browser_helper_functions(browser):
+    browser.get_body_text = lambda: browser.find_element_by_css_selector("body").text
+    browser.login = partial(login, browser)
+    browser.wait_till_gone = partial(wait_till_gone, browser)
+    browser.slowly_click = partial(slowly_click, browser)
+    # TODO : this is going to suck if search_string has " or '
+    browser.assert_body_contains_text = partial(assert_body_contains_text, browser)
+
+
+def goto(browser, server_address, url):
+    if url[:4] == "http":
+        browser.get(url)
+    else:
+        browser.get(server_address + url)
 
 
 @pytest.fixture
@@ -33,26 +77,79 @@ def browser(live_server, settings):
         # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         driver = webdriver.Chrome(options=chrome_options)
-        driver.implicitly_wait(10)  # seconds
+        driver.implicitly_wait(5)  # seconds
 
-        driver.goto = lambda url: driver.get(live_server.url + url)
-        driver.get_body_text = lambda: driver.find_element_by_css_selector("body").text
-        driver.login = lambda username, password: login(driver, username, password)
-        # TODO : this is going to suck if search_string has " or '
-        driver.assert_body_contains_text = lambda search_string: assert_body_contains_text(driver, search_string)
+        driver.goto = partial(goto, driver, live_server.url)
+        setup_browser_helper_functions(driver)
 
         yield driver
     finally:
         driver.quit()
 
 
-User = namedtuple("User", ["username", "password"])
+@pytest.fixture
+def erp(django_user_model):
+    # for the erp to work, there are some users and group permissions that we need
+    django_user_model.objects.create_user("vault")
+
+    grader_group = Group.objects.create(name="grader")
+    permission = Permission.objects.get(
+        codename="view_parcel", content_type=ContentType.objects.get(app_label="grading", model="parcel")
+    )
+    grader_group.permissions.add(permission)
+
+    pass
+
+
+UserData = namedtuple("User", ["username", "password"])
 
 
 @pytest.fixture
 def user(django_user_model):
     # the way that our pytest-django live-server is setup, the db is
     # automatically flushed in between tests
-    user_data = User(username="alice@alice.com", password="alicepassword")
-    django_user_model.objects.create_user(user_data.username, email=user_data.username, password=user_data.password)
-    return user_data
+    user_data = UserData(username="alice@alice.com", password="alicepassword")
+    created_user = django_user_model.objects.create_user(
+        user_data.username, email=user_data.username, password=user_data.password
+    )
+    created_user.raw_password = user_data.password
+    return created_user
+
+
+@pytest.fixture
+def grader(django_user_model):
+    user_data = UserData(username="grader@grader.com", password="graderpassword")
+    user = django_user_model.objects.create_user(
+        user_data.username, email=user_data.username, password=user_data.password, is_staff=True
+    )
+    grader_group = Group.objects.get(name="grader")
+    user.groups.add(grader_group)
+
+    user.raw_password = user_data.password
+    return user
+
+
+@pytest.fixture
+def customer():
+    return Entity.objects.create(name="Van Klaren", address="hk", phone=1, email="vk@vk.com")
+
+
+@pytest.fixture
+def receipt(django_user_model, customer, admin_user):
+    created_receipt = Receipt.objects.create(entity=customer, code="VK-0001", intake_by=admin_user)
+    parcel = Parcel.objects.create(
+        receipt=created_receipt,
+        gradia_parcel_code="parcel1",
+        customer_parcel_code="cust-parcel-1",
+        total_carats=2,
+        total_pieces=2,
+        reference_price_per_carat=1,
+    )
+    # the parcel is received by admin user and put into the vault
+    ParcelTransfer.objects.create(
+        item=parcel, from_user=admin_user, to_user=django_user_model.objects.get(username="vault")
+    )
+    # vault confirms it has received this parcel
+    ParcelTransfer.confirm_received(parcel)
+    created_receipt.parcel_set.add(parcel)
+    return created_receipt
--- selenium_tests/test_admin_page_grader.py
+++ selenium_tests/test_admin_page_grader.py
@@ -1,7 +1,45 @@
-def test_example(browser, admin_user):
-    # needs to be staff to access the ERP page
-    browser.login("admin", "password")
+from django.contrib.auth.models import User
 
-    browser.goto("/admin")
+from ownerships.models import ParcelTransfer
 
-    browser.assert_body_contains_text("Portfolio Overview")
+
+def test_grader_can_confirm_received_stones(browser, erp, grader, receipt):
+    # Tanly is a grader
+    # the vault has given her a parcel to grade
+    parcel = receipt.parcel_set.first()
+    vault = User.objects.get(username="vault")
+    ParcelTransfer.initiate_transfer(parcel, vault, grader)
+
+    # she logs in to the admin portal
+    browser.login(grader.username, grader.raw_password)
+
+    # she has access to view parcels
+    parcel_link = browser.find_element_by_partial_link_text("Parcel")
+    browser.slowly_click(parcel_link)
+
+    # stash this for later
+    parcel_page_url = browser.current_url
+
+    # and sees that there is a parcel when she filters by current owner
+    owner_filter = browser.find_element_by_link_text("With me")
+    browser.slowly_click(owner_filter)
+    browser.find_element_by_partial_link_text(parcel.gradia_parcel_code)
+    # and there is an action button prompting her to confirm she received the parcel
+    confirm_link = browser.find_element_by_link_text("Confirm Received")
+
+    # offline, she goes and weighs and counts the # of stones
+    # after checking that, she clicks confirm
+    browser.slowly_click(confirm_link)
+    # she is taken to an "are you sure page"
+    browser.assert_body_contains_text("You are about to confirm")
+    # she clicks proceed
+    proceed_button = browser.find_element_by_name("proceed")
+    browser.slowly_click(proceed_button)
+
+    # now the parcel shows up as with Tanly, and as confirmed
+    browser.goto(parcel_page_url)
+    owner_filter = browser.find_element_by_link_text("With me")
+    browser.slowly_click(owner_filter)
+    browser.find_element_by_partial_link_text(parcel.gradia_parcel_code)
+    browser.assert_body_contains_text(f"{grader.username}, confirmed")
+    browser.find_element_by_link_text("Return to Vault")
