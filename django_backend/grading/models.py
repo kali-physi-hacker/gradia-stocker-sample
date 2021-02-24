99c8d91c37b2bf5d227229f35ffc47e5acc8a497
Desmond Brown <browndesmond30@yahoo.com>
2021-02-24 08:20:21 +0000

    Refactored Stone models to use grades from the stone-grading package

--- django_backend/grading/models.py
+++ django_backend/grading/models.py
@@ -13,6 +13,10 @@ from django.db.utils import IntegrityError
 from django.urls import reverse
 from django.utils.html import format_html
 
+from stonegrading.grades import (
+    ColorGrades, CuletGrades, ClarityGrades, GeneralGrades, GirdleGrades, FluorescenceGrades, Inclusions
+)
+
 from customers.models import Entity
 from ownerships.models import ParcelTransfer, StoneTransfer
 
@@ -214,9 +218,9 @@ class Parcel(AbstractParcel):
                         f"<a href='{reverse('grading:return_to_vault', args=[self.id])}'>Return to Vault</a>"
                     )
             if (
-                transfer.in_transit()
-                and transfer.to_user.username == "vault"
-                and user.groups.filter(name="vault_manager").exists()
+                    transfer.in_transit()
+                    and transfer.to_user.username == "vault"
+                    and user.groups.filter(name="vault_manager").exists()
             ):
                 return format_html(
                     f"<a href='{reverse('grading:confirm_received', args=[self.id])}'>Confirm Stones for Vault</a>"
@@ -258,183 +262,6 @@ class GiaVerification(models.Model):
 
     def summary(self):
         return f"{self.stone_set.count()} stones"
-
-
-class ColorGrades:
-    COLORLESS_D = "D"
-    COLORLESS_E = "E"
-    COLORLESS_F = "F"
-    NEARLY_COLORLESS_G = "G"
-    NEARLY_COLORLESS_H = "H"
-    NEARLY_COLORLESS_I = "I"
-    NEARLY_COLORLESS_J = "J"
-
-    CHOICES = (
-        (COLORLESS_D, "Colorless D"),
-        (COLORLESS_E, "Colorless E"),
-        (COLORLESS_F, "Colorless F"),
-        (NEARLY_COLORLESS_G, "Nearly Colorless G"),
-        (NEARLY_COLORLESS_H, "Nearly Colorless H"),
-        (NEARLY_COLORLESS_I, "Nearly Colorless I"),
-        (NEARLY_COLORLESS_J, "Nearly Colorless J"),
-    )
-
-
-class ClarityGrades:
-    INTERNALLY_FLAWLESS = "IF"
-    VERY_VERY_SLIGHTLY_INCLUDED_DEGREE_1 = "VVS1"
-    VERY_VERY_SLIGHTLY_INCLUDED_DEGREE_2 = "VVS2"
-    VERY_SLIGHTLY_INCLUDED_DEGREE_1 = "VS1"
-    VERY_SLIGHTLY_INCLUDED_DEGREE_2 = "VS2"
-    SLIGHTLY_INCLUDED_DEGREE_1 = "SI1"
-    SLIGHTLY_INCLUDED_DEGREE_2 = "SI2"
-    INCLUDED = "I1"
-
-    CHOICES = (
-        (INTERNALLY_FLAWLESS, "Internally Flawless"),
-        (VERY_VERY_SLIGHTLY_INCLUDED_DEGREE_1, "Very Very Slightly Included Degree 1"),
-        (VERY_VERY_SLIGHTLY_INCLUDED_DEGREE_2, "Very Very Slightly Included Degree 2"),
-        (VERY_SLIGHTLY_INCLUDED_DEGREE_1, "Very Slightly Included Degree 1"),
-        (VERY_SLIGHTLY_INCLUDED_DEGREE_2, "Very Slightly Included Degree 2"),
-        (SLIGHTLY_INCLUDED_DEGREE_1, "Slightly Included Degree 1"),
-        (SLIGHTLY_INCLUDED_DEGREE_2, "Slightly Included Degree 2"),
-        (INCLUDED, "Included"),
-    )
-
-
-class GeneralGrades:
-    EXCELLENT = "EX"
-    VERY_GOOD = "VG"
-    GOOD = "GD"
-    FAIR = "F"
-    POOR = "P"
-
-    CHOICES = ((EXCELLENT, "Excellent"), (VERY_GOOD, "Very Good"), (GOOD, "Good"), (FAIR, "Fair"), (POOR, "Poor"))
-
-
-class FluorescenceGrades:
-    VERY_STRONG = "VS"
-    STRONG = "S"
-    MEDIUM = "M"
-    FAINT = "F"
-    NONE = "N"
-
-    CHOICES = (
-        (VERY_STRONG, "Very Strong"),
-        (STRONG, "Strong"),
-        (MEDIUM, "Medium"),
-        (FAINT, "Faint"),
-        (NONE, "None"),
-    )
-
-
-class GirdleGrades:
-    EXTREMELY_THIN = "EXT"
-    VERY_THIN = "VTN"
-    THIN = "THIN"
-    MEDIUM = "MED"
-    SLIGHTLY_THICK = "STK"
-    THICK = "THK"
-    VERY_THICK = "VTK"
-    EXTREMELY_THICK = "EXT"
-    FACETED = "F"
-    SMOOTH = "SM"
-    EXTREMELY_THIN_TO_VERY_THIN = "ETN TO VTN"
-
-    CHOICES = (
-        (EXTREMELY_THIN, "Extremely Thin"),
-        (VERY_THIN, "Very Thin"),
-        (THIN, "Thin"),
-        (MEDIUM, "Medium"),
-        (SLIGHTLY_THICK, "Slightly Thick"),
-        (THICK, "Thick"),
-        (VERY_THICK, "Very Thick"),
-        (EXTREMELY_THICK, "Extremely Thick"),
-        (FACETED, "Faceted"),
-        (SMOOTH, "Smooth"),
-        (EXTREMELY_THIN_TO_VERY_THIN, "Extremely thin - very thin"),
-    )
-
-
-class Inclusions:
-    BRUISE = "Br"
-    CAVITY = "Cv"
-    CHIP = "Ch"
-    CLEAVAGE = "Clv"
-    CLOUD = "Cld"
-    CRYSTAL = "Xtl"
-    FEATHER = "Ftr"
-    GRAIN_CENTER = "GrCnt"
-    INDENTED_NATURAL = "IndN"
-    INTERNAL_GRAINING = "IntGr"
-    KNOT = "K"
-    LASER_DRILL_HOLE = "LDH"
-    NEEDLE = "Ndl"
-    PINPOINT = "Pp"
-    TWINNING_WISP = "W"
-    ABRASION = "Abr"
-    NATURAL = "N"
-    NICK = "Nk"
-    PIT = "Pit"
-    POLISH_LINE = "PL"
-    BURN_MASK = "Brn"
-    SCRATCH = "S"
-    SURFACE_GRAINING = "SGr"
-    EXTRA_FEET = "EF"
-
-    CHOICES = (
-        (BRUISE, "Bruise"),
-        (CAVITY, "Cavity"),
-        (CHIP, "Chip"),
-        (CLEAVAGE, "Cleavage"),
-        (CLOUD, "Cloud"),
-        (CRYSTAL, "Xtl"),
-        (FEATHER, "Feather"),
-        (GRAIN_CENTER, "Grain Center"),
-        (INDENTED_NATURAL, "Indented natural"),
-        (INTERNAL_GRAINING, "Internal graining"),
-        (KNOT, "knot"),
-        (LASER_DRILL_HOLE, "Laser Drill Hole"),
-        (NEEDLE, "Needle"),
-        (PINPOINT, "Pinpoint"),
-        (TWINNING_WISP, "Twinning Wisp"),
-        (ABRASION, "Abrasion"),
-        (NATURAL, "Natural"),
-        (NICK, "Nick"),
-        (PIT, "Pit"),
-        (POLISH_LINE, "Polish Line"),
-        (BURN_MASK, "Burn Mask"),
-        (SCRATCH, "Scratch"),
-        (SURFACE_GRAINING, "Surface Graining"),
-        (EXTRA_FEET, "Extra Feet"),
-    )
-
-
-class CuletGrades:
-    NONE = "N"
-    VERY_SMALL = "VS"
-    SMALL = "S"
-    MEDIUM = "M"
-    SLIGHTLY_LARGE = "SL"
-    LARGE = "L"
-    VERY_LARGE = "VL"
-    EXTREMELY_LARGE = "XL"
-
-    CHOICES = (
-        (NONE, "None"),
-        (VERY_SMALL, "Very Small"),
-        (SMALL, "Small"),
-        (MEDIUM, "Medium"),
-        (SLIGHTLY_LARGE, "Slightly Large"),
-        (LARGE, "Large"),
-        (VERY_LARGE, "Very Large"),
-        (EXTREMELY_LARGE, "Extremely Large"),
-    )
-
-    MULTI_CHOICES = list(itertools.combinations(CHOICES, 2))
-    MULTI_CHOICES = [list(item) for item in MULTI_CHOICES]
-    MULTI_CHOICES = [sorted(item) for item in MULTI_CHOICES]
-    MULTI_CHOICES = tuple((f"{a[0]}/{b[0]}", f"{a[1]} / {b[1]}") for (a, b) in MULTI_CHOICES)
 
 
 class Inclusion(models.Model):
