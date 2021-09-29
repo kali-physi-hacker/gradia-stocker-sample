cf9939460abbb3c9cad55cf51c2443f1ff750e70
Efoblack <xplicitbwoy212@gmail.com>
2021-09-29 17:42:48 +0000

    selenium test for gia data upload csv

--- selenium_tests/grading_fixtures.py
+++ selenium_tests/grading_fixtures.py
@@ -412,3 +412,91 @@ def parcels(admin_user):
     )
 
     return (valid_csv_parcel, invalid_dtype_csv)
+
+
+def setup_initial_stones(data_entry_user, split_from, receipt):
+    parcel = receipt.parcel_set.get(gradia_parcel_code="123456789")
+    split = Split.objects.create(original_parcel=parcel, split_by=data_entry_user)
+    sarine_fields = {
+        "diameter_min": 2.86,
+        "diameter_max": 2.9,
+        "height": 1.77,
+        "table_size": 60.2,
+        "crown_angle": 34.75,
+        "pavilion_angle": 41.89,
+        "star_length": 51,
+        "lower_half": 74.3,
+        "girdle_thickness_number": 3.09,
+        "girdle_min_number": 0.69,
+        "girdle_max_number": 2.08,
+        "culet_size": 0.59,
+        "crown_height": 13.85,
+        "pavilion_depth": 44.53,
+        "total_depth": 61.51,
+        "table_size_rounded": 60,
+        "crown_angle_rounded": 35,
+        "pavilion_angle_rounded": 41.8,
+        "star_length_rounded": 50,
+        "lower_half_rounded": 75,
+        "girdle_thickness_rounded": 3,
+        "girdle_min_grade": "ETN TO VTN",
+        "girdle_max_grade": "STK",
+        "culet_size_description": "N/VS",
+        "crown_height_rounded": 14,
+        "pavilion_depth_rounded": 44.5,
+        "total_depth_rounded": 61.5,
+        "sarine_cut_pre_polish_symmetry": "VG",
+        "sarine_symmetry": "VG",
+        "roundness": 1.3,
+        "roundness_grade": "VG",
+        "table_size_dev": 1,
+        "table_size_dev_grade": "EX",
+        "crown_angle_dev": 0.6,
+        "crown_angle_dev_grade": "EX",
+        "pavilion_angle_dev": 1,
+        "pavilion_angle_dev_grade": "VG",
+        "star_length_dev": 6.3,
+        "star_length_dev_grade": "EX",
+        "lower_half_dev": 3.2,
+        "lower_half_dev_grade": "EX",
+        "girdle_thick_dev": 1.4,
+        "girdle_thick_dev_grade": "VG",
+        "crown_height_dev": 1,
+        "crown_height_dev_grade": "EX",
+        "pavilion_depth_dev": 1.4,
+        "pavilion_depth_dev_grade": "VG",
+        "misalignment": 1.8,
+        "misalignment_grade": "VG",
+        "table_edge_var": 1.8,
+        "table_edge_var_grade": "EX",
+        "table_off_center": 0.5,
+        "table_off_center_grade": "EX",
+        "culet_off_center": 1.2,
+        "culet_off_center_grade": "VG",
+        "table_off_culet": 1.7,
+        "table_off_culet_grade": "VG",
+        "star_angle": 2.6,
+        "star_angle_grade": "EX",
+        "upper_half_angle": 2.9,
+        "upper_half_angle_grade": "EX",
+        "lower_half_angle": 1.3,
+        "lower_half_angle_grade": "EX",
+    }
+    internal_ids = (1, 5, 6)
+
+    stones = []
+
+    for internal_id in internal_ids:
+        sarine_fields["internal_id"] = internal_id
+        sarine_fields["data_entry_user"] = data_entry_user
+        sarine_fields["split_from"] = split
+        stones.append(Stone.objects.create(**sarine_fields))
+
+    return stones
+
+
+@pytest.fixture
+def initial_stones(django_user_model, data_entry_clerk, receipt, erp):
+    return setup_initial_stones(
+        data_entry_user=data_entry_clerk, receipt=receipt, split_from=django_user_model.objects.get(username="split")
+    )
