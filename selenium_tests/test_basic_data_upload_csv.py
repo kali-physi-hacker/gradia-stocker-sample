afcb50b8f11a9c0a5b96eba262b8ae166770cbc3
Efoblack <xplicitbwoy212@gmail.com>
2021-09-30 09:56:23 +0000

    linting

--- selenium_tests/test_basic_data_upload_csv.py
+++ selenium_tests/test_basic_data_upload_csv.py
@@ -25,8 +25,9 @@ def test_user_can_upload_basic_stone_data(browser, data_entry_clerk, tanly, gary
     assert re.match(r"^http://localhost:\d+/admin/grading/split/\d+/change/", browser.current_url) is not None
 
 
-
-def test_upload_fails_if_invalid_basic_inclusions_field(browser, data_entry_clerk, tanly, gary, kary, initial_stones):
+def test_upload_fails_if_invalid_basic_inclusions_field(
+    browser, data_entry_clerk, tanly, gary, kary, initial_stones
+):
 
     browser.login(data_entry_clerk.username, data_entry_clerk.raw_password)
 
@@ -47,11 +48,15 @@ def test_upload_fails_if_invalid_basic_inclusions_field(browser, data_entry_cler
     browser.find_element_by_name("_upload").click()
 
     browser.assert_body_contains_text("Enter a list of values.")
-    browser.assert_body_contains_text("Stone csv data upload failed. Check the table below to find out the problems with your csv file.")
+    browser.assert_body_contains_text(
+        "Stone csv data upload failed. Check the table below to find out the problems with your csv file."
+    )
 
 
-def test_basic_data_upload_fails_if_invalid_data_type(browser, data_entry_clerk, tanly, gary, kary, initial_stones, inclusions):
-    
+def test_basic_data_upload_fails_if_invalid_data_type(
+    browser, data_entry_clerk, tanly, gary, kary, initial_stones, inclusions
+):
+
     browser.login(data_entry_clerk.username, data_entry_clerk.raw_password)
 
     link = browser.find_element_by_link_text("Splits")
@@ -70,6 +75,8 @@ def test_basic_data_upload_fails_if_invalid_data_type(browser, data_entry_clerk,
 
     browser.find_element_by_name("_upload").click()
 
-    browser.assert_body_contains_text("Stone csv data upload failed. Check the table below to find out the problems with your csv file.")
+    browser.assert_body_contains_text(
+        "Stone csv data upload failed. Check the table below to find out the problems with your csv file."
+    )
     browser.assert_body_contains_text("Enter a number.")
     browser.assert_body_contains_text("Select a valid choice. 9.0 is not one of the available choices.")
