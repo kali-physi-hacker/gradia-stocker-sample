d45087daab0c4302441ee027849704c8a0e934a5
Kali Junior Brown <browndesmond30@yahoo.com>
2022-01-06 12:41:28 +0000

    Implemented tests for helper Stone model properties added (is_basic_grading_complete, etc)

--- django_backend/grading/tests/test_model_functions.py
+++ django_backend/grading/tests/test_model_functions.py
@@ -1,101 +1,228 @@
-# import os
-# import csv
-# from django.test import TestCase
-# from grading.models import Stone
-# from grading.helpers import get_stone_fields
-#
-#
-# class TestCSVGeneration(TestCase):
-#     fixtures = ("data_migration/test_data.json",)
-#
-#     def setUp(self):
-#         self.stone1 = Stone.objects.get(internal_id=1)
-#         self.stone2 = Stone.objects.get(internal_id=2)
-#         self.stone3 = Stone.objects.get(internal_id=3)
-#
-#     def test_generated_id_csv(self):
-#         """
-#         Send File and 200 response status if report id (gradia_ID)
-#         is valid and exists
-#         Returns:
-#         """
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_id_csv(queryset=queryset)
-#         self.assertTrue(os.path.exists(file_path))
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             self.assertIn("internal_id", reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertEqual(3, len(csv_list))
-#         self.assertEqual(str(self.stone3.internal_id), csv_list[0]["internal_id"])
-#         self.assertEqual(str(self.stone1.internal_id), csv_list[2]["internal_id"])
-#
-#     def test_generated_master_report_csv(self):
-#
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_master_report_csv(queryset=queryset)
-#
-#         field_names = get_stone_fields(Stone)
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             for field in field_names:
-#                 self.assertIn(field, reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertEqual(3, len(csv_list))
-#         import pdb; pdb.set_trace()
-#         self.assertEqual(str(self.stone3.data_entry_user), csv_list[0]["data_entry_user"])
-#         self.assertEqual(str(self.stone1.date_created), csv_list[2]["date_created"])
-#
-#     def test_generated_to_goldway_csv(self):
-#
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_to_goldway_csv(queryset=queryset)
-#         field_names = ["date_to_GW", "internal_id", "basic_carat"]
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             for field in field_names:
-#                 self.assertIn(field, reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertEqual(3, len(csv_list))
-#         self.assertEqual(str(self.stone3.basic_carat), csv_list[0]["basic_carat"])
-#         self.assertEqual(str(self.stone1.internal_id), csv_list[2]["internal_id"])
-#
-#     def test_generated_to_GIA_csv(self):
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_to_GIA_csv(queryset=queryset)
-#         field_names = ["date_to_GIA", "external_id", "carat_weight", "color"]
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             for field in field_names:
-#                 self.assertIn(field, reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertTrue(len(csv_list) == 3)
-#         self.assertEqual(str(self.stone3.external_id), csv_list[0]["external_id"])
-#         self.assertEqual(str(self.stone1.carat_weight), csv_list[2]["carat_weight"])
-#
-#     def test_generated_basic_report_csv(self):
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_basic_report_csv(queryset=queryset)
-#         field_names = Stone.basic_grading_fields
-#
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             for field in field_names:
-#                 self.assertIn(field, reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertTrue(len(csv_list) == 3)
-#         self.assertEqual(str(self.stone3.total_depth), csv_list[0]["total_depth"])
-#         self.assertEqual(str(self.stone1.inclusions), csv_list[2]["inclusions"])
-#
-#     def test_generated_triple_report_csv(self):
-#         queryset = Stone.objects.all()
-#         file_path = Stone.objects.generate_triple_report_csv(queryset=queryset)
-#         field_names = ["internal_id", "external_id", "carat_weight", "color", "clarity"]
-#         with open(file_path, "r") as file:
-#             reader = csv.DictReader(file)
-#             for field in field_names:
-#                 self.assertIn(field, reader.fieldnames)
-#             csv_list = list(reader)
-#         self.assertTrue(len(csv_list) == 3)
-#         self.assertEqual(str(self.stone3.carat_weight), csv_list[0]["carat_weight"])
-#         self.assertEqual(str(self.stone1.color), csv_list[2]["color"])
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth import get_user_model
+
+from grading.models import Stone
+from grading.forms import (
+    SarineUploadForm,
+    BasicUploadForm,
+    GWGradingUploadForm,
+    GIAUploadForm,
+    GWAdjustingUploadForm,
+    GIAAdjustingUploadForm,
+)
+
+from ownerships.models import StoneTransfer
+
+
+User = get_user_model()
+
+
+class StoneModelTest(TestCase):
+    fixtures = ("grading/fixtures/test_data.json",)
+
+    def setUp(self):
+        self.grader = User.objects.get(username="tanly")
+        self.split_user = User.objects.get(username="split")
+        self.goldway_user = User.objects.get(username="goldway")
+        self.gia_user = User.objects.get(username="gia")
+        self.vault_manager = User.objects.get(username="vault")
+
+        self.stone_ids = (1, 5, 6)
+
+    def transfer_stones(self, to_user, from_user):
+        """
+        Transfer stones
+        :param to_user:
+        :param from_user:
+        :return:
+        """
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            StoneTransfer.initiate_transfer(item=stone, from_user=from_user, to_user=to_user, created_by=self.grader)
+            StoneTransfer.confirm_received(item=stone)
+
+    def upload_sarine_grading_results(self):
+        """
+        Uploads sarine data
+        :return:
+        """
+        with open("grading/tests/fixtures/sarine-01.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = SarineUploadForm(data={}, files={"file": uploaded_file}, user=self.grader)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def upload_basic_grading_results(self):
+        """
+        Upload basic grading results
+        :return:
+        """
+        with open("grading/tests/fixtures/basic-01.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = BasicUploadForm(data={}, files={"file": uploaded_file}, user=self.grader)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def upload_goldway_grading_results(self):
+        """
+        Upload goldway grading results
+        :return:
+        """
+        with open("grading/tests/fixtures/gold_way-01.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = GWGradingUploadForm(data={}, files={"file": uploaded_file}, user=self.grader)
+        self.transfer_stones(from_user=self.split_user, to_user=self.goldway_user)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def upload_goldway_adjusting_grading_results(self):
+        """
+        Upload goldway adjusting grading results
+        :return:
+        """
+        with open("grading/tests/fixtures/gw_adjust.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = GWAdjustingUploadForm(data={}, files={"file": uploaded_file})
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def upload_gia_grading_results(self):
+        """
+        Upload gia grading results
+        :return:
+        """
+        with open("grading/tests/fixtures/gia.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = GIAUploadForm(data={}, files={"file": uploaded_file}, user=self.grader)
+
+        self.transfer_stones(from_user=self.vault_manager, to_user=self.gia_user)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def upload_gia_adjusting_grading_results(self):
+        """
+        Upload gia adjusting grading results
+        :return:
+        """
+        with open("grading/tests/fixtures/gia_adjusting.csv", "rb") as file:
+            uploaded_file = SimpleUploadedFile(file.name, file.read())
+
+        form = GIAAdjustingUploadForm(
+            data={},
+            files={"file": uploaded_file},
+        )
+
+        self.assertTrue(form.is_valid())
+        form.save()
+
+    def test_is_sarine_grading_complete(self):
+        """
+        Tests that is_sarine_grading_complete returns True
+        :return:
+        """
+        self.upload_sarine_grading_results()
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_sarine_grading_complete)
+
+    def test_is_basic_grading_complete(self):
+        """
+        Tests that is_basic_grading_complete returns True if basic grading results
+        have been uploaded and returns False otherwise
+        :return:
+        """
+        self.upload_sarine_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertFalse(stone.is_basic_grading_complete)
+
+        self.upload_basic_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_basic_grading_complete)
+
+    def test_is_goldway_grading_complete(self):
+        """
+        Tests that is_goldway_grading_complete returns True if goldway grading results
+        have been uploaded and returns False otherwise
+        :return:
+        """
+        self.upload_sarine_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertFalse(stone.is_goldway_grading_complete)
+
+        self.upload_goldway_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_goldway_grading_complete)
+
+    def test_is_goldway_adjusting_grading_complete(self):
+        """
+        Tests that is_goldway_adjusting_complete returns True if goldway adjusting results
+        have been uploaded and returns False otherwise
+        :return:
+        """
+        self.upload_sarine_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertFalse(stone.is_goldway_adjusting_grading_complete)
+
+        self.upload_goldway_adjusting_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_goldway_adjusting_grading_complete)
+
+    def test_is_gia_grading_complete(self):
+        """
+        Tests that is_gia_grading_complete returns True if gia grading results
+        have been uploaded and returns False otherwise
+        :return:
+        """
+        self.upload_sarine_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertFalse(stone.is_gia_grading_complete)
+
+        self.upload_goldway_grading_results()
+        self.upload_gia_grading_results()
+
+        import pdb
+
+        pdb.set_trace()
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_gia_grading_complete)
+
+    def test_is_gia_adjusting_grading_complete(self):
+        """
+        Tests that is_gia_adjusting_grading_complete returns True if gia adjusting results
+        have been uploaded and returns False otherwise
+        :return:
+        """
+        self.upload_sarine_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertFalse(stone.is_gia_adjusting_grading_complete)
+
+        self.upload_goldway_grading_results()
+        self.upload_gia_grading_results()
+        self.upload_gia_adjusting_grading_results()
+
+        for stone_id in self.stone_ids:
+            stone = Stone.objects.get(internal_id=stone_id)
+            self.assertTrue(stone.is_gia_adjusting_grading_complete)
