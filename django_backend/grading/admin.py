aa7e66a3fb914a38e3bdbbdcb45665b675a6e222
xplicitbwoy212@gmail.com <xplicitbwoy212@gmail.com>
2022-02-03 12:54:27 +0000

    lint formatting

--- django_backend/grading/admin.py
+++ django_backend/grading/admin.py
@@ -276,9 +276,8 @@ class StoneAdmin(admin.ModelAdmin):
     list_filter = [StoneOwnerFilter]
 
     search_fields = ["split_from__original_parcel__receipt__code"]
-    
-    change_list_template = "grading/stone_data_upload.html"
 
+    change_list_template = "grading/stone_data_upload.html"
 
     def get_list_display(self, request):
         return [
