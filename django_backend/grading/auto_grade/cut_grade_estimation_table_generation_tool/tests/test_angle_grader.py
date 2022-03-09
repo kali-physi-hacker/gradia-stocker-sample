664212800677443541b8e28529e98b3895fd1cc9
Kali Junior Brown <browndesmond30@yahoo.com>
2022-03-09 18:00:16 +0000

    Renamed table ds generator to a more descriptive and appropriate name

--- django_backend/grading/auto_grade/cut_grade_estimation_table_generation_tool/tests/test_angle_grader.py
+++ django_backend/grading/auto_grade/cut_grade_estimation_table_generation_tool/tests/test_angle_grader.py
@@ -3,19 +3,20 @@ import unittest
 import os
 
 import numpy as np
+
 # Parent Module Import Setup
 sys.path.append(".")
 print(sys.path)
-from ..core.table_ds_generator import TableDSGenerator
+from ..core.angle_grader import DiamondAngleGrader
 
 
-class TableDSGeneratorTest(unittest.TestCase):
+class DiamondAngleGraderTest(unittest.TestCase):
     def setUp(self):
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         table_5_path = os.path.join(BASE_DIR, "test_grade_tables", "5.txt")
         table_3_path = os.path.join(BASE_DIR, "test_grade_tables", "3.txt")
-        self.table_5 = TableDSGenerator(table_file_path=table_5_path)
-        self.table_3 = TableDSGenerator(table_file_path=table_3_path)
+        self.table_5 = DiamondAngleGrader(table_file_path=table_5_path)
+        self.table_3 = DiamondAngleGrader(table_file_path=table_3_path)
         self.table_file_path = os.path.join("tests", "test_grade_tables")
 
     def test_table_generates_ds_given_valid_input_file(self):
@@ -40,7 +41,6 @@ class TableDSGeneratorTest(unittest.TestCase):
         self.assertEqual(calculated_grade_4, expected_grade_3)
         self.assertEqual(calculated_grade_5, expected_grade)
 
-
     def test_table_raises_value_error_if_invalid_angles(self):
         """
         Tests that `get_grade` raises value error if invalid angles are supplied as arguments
@@ -48,15 +48,16 @@ class TableDSGeneratorTest(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             self.table_3.get_grade(999, 240)
-            self.table_3.get_grade(22.0, 38.8)
+
+        with self.assertRaises(ValueError):
+            self.table_3.get_grade(62.0, 38.8)
+
+        with self.assertRaises(ValueError):
             self.table_3.get_grade(40.0, 43.0)
-            
+
     def test_empty_table_raises_error(self):
         with self.assertRaises(Exception):
             table_1_path = os.path.join(BASE_DIR, "test_grade_tables", "1.txt")
-            self.table_1 = TableDSGenerator(table_file_path=table_1_path)
+            self.table_1 = DiamondAngleGrader(table_file_path=table_1_path)
+
     # Jason you can try working on more test cases. Just try thinking of possible edges (happy / sad cases)
-
-
-if __name__ == "__main__":
-    unittest.main()
