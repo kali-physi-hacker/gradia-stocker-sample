8c01a282da111b4acb82b3db1e80d94a65ac698d
Desmond Brown <browndesmond30@yahoo.com>
2022-03-28 13:41:47 +0000

    Implemented selenium test for preventing uploading gia_adjust results twice for the same stone

--- selenium_tests/test_gia_adjust_data_upload.py
+++ selenium_tests/test_gia_adjust_data_upload.py
@@ -39,3 +39,39 @@ def test_user_upload_gia_fails_if_invalid_csv_file(browser, data_entry_clerk, ta
     assert re.match(r"^http://localhost:\d+/grading/upload/gia_adjusting/", browser.current_url) is not None
     browser.assert_body_contains_text("Data Upload Failed")
     browser.assert_body_contains_text("Grader user `boo` account does not exist")  # Some error message
+
+
+def test_user_cannot_upload_gia_adjust_grading_results_twice_for_the_same_stone(browser, data_entry_clerk, tanly, gary, kary, initial_stones):
+    # 1st Upload
+    browser.login(username=data_entry_clerk.username, password=data_entry_clerk.raw_password)
+
+    browser.find_element_by_link_text("Stones").click()
+    browser.find_element_by_link_text("UPLOAD STONE DATA").click()
+
+    browser.find_element_by_link_text("Gia Adjust Grading Data").click()
+
+    os.chdir("../django_backend")
+    gia_csv_file_path = os.path.join(os.getcwd(), "grading/tests/fixtures/gia_adjusting.csv")
+    upload_file_input = browser.find_element_by_name("file")
+    upload_file_input.send_keys(gia_csv_file_path)
+
+    browser.find_element_by_name("_upload").click()
+
+    assert re.match(r"^http://localhost:\d+/admin/grading/split/\d+/change/", browser.current_url) is not None
+
+    # 2nd Upload 
+    browser.find_element_by_link_text("Splits").click()
+
+    browser.find_element_by_link_text("Stones").click()
+    browser.find_element_by_link_text("UPLOAD STONE DATA").click()
+
+    browser.find_element_by_link_text("Gia Adjust Grading Data").click()
+
+    os.chdir("../django_backend")
+    gia_csv_file_path = os.path.join(os.getcwd(), "grading/tests/fixtures/gia_adjusting.csv")
+    upload_file_input = browser.find_element_by_name("file")
+    upload_file_input.send_keys(gia_csv_file_path)
+
+    browser.find_element_by_name("_upload").click()
+
+    assert re.match(r"^http://localhost:\d+/admin/grading/split/\d+/change/", browser.current_url) is not None
